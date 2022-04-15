d2c6d61408901ae83beb01b3765dde2cddb604b0
cometbroom <alawi.wow@gmail.com>
2022-04-15 15:30:11 +0200

    Fixed response format, explicitly set content-type, added comments

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,19 +1,24 @@
 import express from "express";
 
+//Start app
 const app = express();
 
+//Middleware to have requests as JSON
 app.use("/weather", express.json());
 
+//Default path
 app.get("/", (req, res) => {
+  res.type("html");
   res.status(200).send("<p>Hello from backend to frontend!</p>");
 });
 
+//Post city name in the body
 app.post("/weather", (req, res) => {
   if (req.body?.cityName) {
     const cityName = req.body.cityName;
-    res.status(200).send(cityName);
+    res.status(200).json({ cityName });
   } else {
-    res.status(404).send("Could not find city in request");
+    res.status(400).send("Could not find city in request");
   }
 });
 
